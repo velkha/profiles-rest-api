4791f399f57eb25bfcb18b05ad7bc963ef5971b2
Velkha <velkapb99@gmail.com>
2024-06-02 17:48:24 +0200

    postman & post

--- profiles_api/endpoints/custom_view.py
+++ profiles_api/endpoints/custom_view.py
@@ -1,12 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+#import custom_serializer
+from profiles_api.serializers import custom_serializer
 
 class CustomView(APIView):
+    serializer_class = custom_serializer.CustomSerializer
+    
     def get(self, request, format=None):
         return Response({'message': 'Hello, World!', 'an_apiview': 'This is a custom view'})
     
     def post(self, request):
-        return Response({'message': 'This is a POST request'})
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            return Response({'message': f'Hello, {name}!'})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     def put(self, request):
         return Response({'message': 'This is a PUT request'})
